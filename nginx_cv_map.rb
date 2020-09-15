9b4bfbc45c8236f46e627a58f83c7425646bc143
Eric Ryan <ewryan@gmail.com>
2020-09-14 23:45:21 -0600

    completed working functionality for v1. comparing results vs last run.

--- nginx_cv_map.rb
+++ nginx_cv_map.rb
@@ -28,6 +28,7 @@ def send_slack_message message
 end
 
 def fetch_nginx_logs_from_remote
+    puts "Fetching nginx logs from remote: #{@secrets_hash["host"]}"
     ssh_result = ""
     Net::SSH.start(@secrets_hash["host"], @secrets_hash["user"], :password => @secrets_hash["password"]) do |ssh|
         result = ssh.exec!("echo \"#{@secrets_hash["password"]}\" | sudo -S zcat -f /var/log/nginx/access.log*")
@@ -37,8 +38,9 @@ def fetch_nginx_logs_from_remote
 end
 
 def match_cv_records input
+    puts "Searching for matching records."
     matched_records = [] #[{:timestamp => "1234", ip => "1.1.1.1"}, ...]
-    input.split("\n").each do |line|                   #uncomment this line to use ssh result
+    input.split("\n").each do |line|                   
         tokenized_line = line.split
         if (tokenized_line[6] == "/cv/" || tokenized_line[6] == "/cv") && tokenized_line[5] == "\"GET"
             matched_records << {:timestamp => tokenized_line[3].tr('[', ''), :ip => tokenized_line[0]} 
@@ -51,22 +53,56 @@ def geo_api_lookup pair
     uri = URI.parse("https://json.geoiplookup.io/#{pair[:ip]}")
     response = Net::HTTP.get_response(uri)
     response_json = JSON.parse(response.body)
-    {:timestamp => pair[:timestamp], :ip => response_json["ip"], :city => response_json["city"], :region => response_json["region"], :country_name => response_json["country_name"]}
+    {"timestamp" => pair[:timestamp], "ip" => response_json["ip"], "city" => response_json["city"], "region" => response_json["region"], "country_name" => response_json["country_name"]}
+end
+
+def read_previous_results 
+    filename = "previous_results.json"
+    if File.exist?(filename)
+        file = File.open filename
+        return JSON.load file
+    else 
+        return []
+    end
+    
+end
+
+def write_result data
+    File.write('./previous_results.json', JSON.dump(data))
+end
+
+def new_records_exist? latest_results, previous_results
+    (latest_results - previous_results).size != 0
 end
 
 #log_strings = File.open("test.log")
 log_strings = fetch_nginx_logs_from_remote
 matched_records = match_cv_records(log_strings)
 
-final_results = []
+latest_results = []
 matched_records.each do |pair|
     # puts "Processing pair: #{pair.inspect}"
     response_hash = geo_api_lookup(pair)
-    final_results << response_hash
+    latest_results << response_hash
 
-    break #Uncomment this debug statement to stop after first loop
+    # break #Uncomment this debug statement to stop after first loop
 
     sleep 0.5
 end
 
-send_slack_message("New GET record(s) for 'http://ericwryan.com/cv/': \n #{final_results.join("\n")}")
+previous_results = read_previous_results
+if new_records_exist? latest_results, previous_results
+    formatted_results = latest_results.join("\n")
+    puts "Found new records. Sending notifying Slack."
+    puts "Latest records dump: #{formatted_results.inspect}"
+    send_slack_message("New GET record(s) for 'http://ericwryan.com/cv/': \n #{formatted_results}")
+else
+    puts "No new records found."
+end
+
+puts "Writing latest results."
+write_result latest_results
+
+puts "Done. Exiting."
+exit
+
